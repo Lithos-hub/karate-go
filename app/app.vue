8deb79754c61d21d68270020bef64767ced8d8e7
Lithos <lithos.contact@gmail.com>
2026-01-07 12:17:24 +0100

    clean code, set colors and seo metadata

--- app/app.vue
+++ app/app.vue
@@ -4,6 +4,12 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Karate-Go | La web para aprender todo sobre el Karate',
+  description:
+    'Karate-Go es una web para aprender todo sobre el Karate, con videos, artículos, cursos y mucho más.',
+})
+</script>
 
 <style scoped></style>
